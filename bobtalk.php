469ad493f029d408697748c1e40fd74c6d4b0dad
alexandrajulius <alexandra.julius@gmail.com>
2020-11-28 11:48:38 +0100

    temp

--- bobtalk.php
+++ bobtalk.php
@@ -4,11 +4,18 @@ require 'src/Bob.php';
 require 'src/WikiQuotes.php';
 require 'src/SamuelBeckett.php';
 
-echo 'Hello, I\'m Bob.' . PHP_EOL;
+
+echo '
+         /       
+       [^_^]   
+       /[_]\ 
+        ] [   
+' . PHP_EOL;
+echo '[^_^] Hello, I\'m Bob.' . PHP_EOL;
 
 $bob = new Bob(new SamuelBeckett(new WikiQuotes()));
 
 while (true) {
 	$sentence = readline();
-    echo $bob->reply($sentence) . PHP_EOL;
+    echo '[^_^] '. $bob->reply($sentence) . PHP_EOL;
 }
